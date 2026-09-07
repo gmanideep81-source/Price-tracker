5c9b1eee59161b87bc92e83c5fca2e861ad42d12
Manideep <gmanideep81@gmail.com>
2026-09-07 18:31:04 +0530

    Add GitHub Actions workflow

--- scraper.py
+++ scraper.py
@@ -10,6 +10,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 chrome_options=webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 driver=webdriver.Chrome(options=chrome_options)
 EMAIL=os.getenv('EMAIL')
 PASSWORD=os.getenv('PASSWORD')
